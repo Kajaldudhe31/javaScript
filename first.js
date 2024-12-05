383a3cbabeaacab7d8591308e7da9780ae7210ab
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2024-12-05 09:57:19 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+a=123;
+console.log(a) 
+let fullName = "Kajal Dudhe"
+console.log(fullName)
+
+const age = 26;
+console.log(age)
+
+const y = BigInt("1224214")
+console.log(y)
+
+var z = Symbol("hello")
+console.log(z)

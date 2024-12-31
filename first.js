9e789646d33ea1fb803b7aafc63d6698956690ec
stpl-kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2024-12-31 17:27:49 +0530

    changes in js file

--- first.js
+++ first.js
@@ -1,8 +1,8 @@
 // console.log used for print the value
-a=123;
-console.log(a) 
+a = 123;
+console.log(a)
 let fullName = "Kajal Dudhe"
-console.log(fullName)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+console.log(fullName)
 const age = 26;
 console.log(age)
 
@@ -12,7 +12,7 @@ console.log(isEdit)
 const Test = null
 console.log(Test)
 
-let mark; 
+let mark;
 console.log(mark)
 
 const y = BigInt("1224214")
@@ -23,69 +23,67 @@ console.log(z)
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Lets print Object
 
-const student ={
-    studName :'Jiaxu',
-    class    : 10,
-    mark     : 88,
-    age      : 15,
+const student = {
+    studName: 'Jiaxu',
+    class: 10,
+    mark: 88,
+    age: 15,
 }
 
 console.log(student) //all object show in student variable      
 
-student['age'] = student['age']+5 //20
+student['age'] = student['age'] + 5 //20
 // student['age']Value assign to variable and add value whatever you want
 console.log(student.age)
-student['studName']="Kajal"
+student['studName'] = "Kajal"
 //Update value assign to variable student
-console.log(student['studName'])    
+console.log(student['studName'])
 
-const product={
-    Milk : 'Katraj Dairy',
-    Types : 'Cow , Buffalo',
-    Dairy$Product : 'Panner,Cheese,Ghee,Curd',
-    FAT_SNF:0.5
+const product = {
+    Milk: 'Katraj Dairy',
+    Types: 'Cow , Buffalo',
+    Dairy$Product: 'Panner,Cheese,Ghee,Curd',
+    FAT_SNF: 0.5
 }
 console.log(product)
 console.log(product['Types'])
 product['FAT_SNF'] = product['FAT_SNF'] + 0.6;
 console.log(product['FAT_SNF'])
 
-product['Milk']="Milk Dairy Name"
+product['Milk'] = "Milk Dairy Name"
 console.log(product['Milk'])
 
-product ['Dairy$Product'] = [{panner:'any',Cheese:'Milk',Ghee:'Loni',Curd:'C vitamin',Rasmalai:'Sweet Dish',Aamarkhand:'Type of Curd'}]
+product['Dairy$Product'] = [{ panner: 'any', Cheese: 'Milk', Ghee: 'Loni', Curd: 'C vitamin', Rasmalai: 'Sweet Dish', Aamarkhand: 'Type of Curd' }]
 console.log(product['Dairy$Product'])
 // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
 const products = {
-    productName:'Ball Pen',
-    rating:4,
-    offer:5,
-    price:270,
-    isDeal:true,
+    productName: 'Ball Pen',
+    rating: 4,
+    offer: 5,
+    price: 270,
+    isDeal: true,
 }
 console.log(products)
-products['productName']='blue pen'
+products['productName'] = 'blue pen'
 console.log(products.productName);
 
-products['rating']=products['rating']+1
+products['rating'] = products['rating'] + 1
 console.log(products['rating'])
 
-products['offer'] = products['offer']+5
+products['offer'] = products['offer'] + 5
 console.log(products.offer)
-products['isDeal']=false
+products['isDeal'] = false
 console.log(products.isDeal)
 
-products['price'] = products['price']+20
+products['price'] = products['price'] + 20
 console.log(products.price)
 
 
 const profile = {
-    userName : "Kajal Dudhe",
-    isFollow :false,
-    followers :44,
-    following :4
+    userName: "Kajal Dudhe",
+    isFollow: false,
+    followers: 44,
+    following: 4
 }
 console.log(profile)
 console.log(typeof profile['isFollow'])
@@ -95,15 +93,15 @@ console.log(typeof profile.userName)
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Arithmetic opertation
 //  (+,-,*,/,%,**)(Unary Operator (Post Increment a++,a--),(Pre Increment)++a,--a)
-const s=14 ,d=3;
-console.log("s =",s ,"d =",d)
-console.log("s+d =",s+d)
-console.log("s-d =",s-d)
-console.log("s*d =",s*d)
-console.log("s/d =",s/d)
-console.log("s%d =",s%d)
-console.log("s**d =",s**d)
-                                
+const s = 14, d = 3;
+console.log("s =", s, "d =", d)
+console.log("s+d =", s + d)
+console.log("s-d =", s - d)
+console.log("s*d =", s * d)
+console.log("s/d =", s / d)
+console.log("s%d =", s % d)
+console.log("s**d =", s ** d)
+
 let e = 3
 let f = 2
 let c = e + f //e and f variable value save to c variable
@@ -130,7 +128,7 @@ console.log(f--)
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Assignment operator(+=,-=,*=,/=,%=,**=)
-let Z = 12 , V = 2
+let Z = 12, V = 2
 console.log("Z += V =", Z += V)//14
 // console.log("Z -= V =", Z -= V)10
 // console.log("Z *= V =", Z *= V)24
@@ -143,13 +141,13 @@ console.log("Z += V =", Z += V)//14
 // Comparision Operator(==,!=,===,!==,< , <=,> , >=)
 const A = 10, B = 5//A = 5, B = 5 & A = 5, B = '5'
 console.log("A == B", A == B)//false in double equal to checking only Value 
-console.log("A != B =",A != B)//True (return, condition is true then not equal to return False and when condition false not equal to return true)
-console.log("A === B =",A === B)//false in tripple equal to checking Value as well as Datatype(checking condition stricktly)
+console.log("A != B =", A != B)//True (return, condition is true then not equal to return False and when condition false not equal to return true)
+console.log("A === B =", A === B)//false in tripple equal to checking Value as well as Datatype(checking condition stricktly)
 console.log("A !== B", A !== B)//True(because this is a correct statement 10 is not equal to 5 valuewise and datatypewise also) 
-console.log("A < B",A < B)//false
+console.log("A < B", A < B)//false
 console.log("A <= B", A <= B)//false (if value is greater than or equal one possibility have i <= condition)
-console.log("A > B",A > B)//true
-console.log("A >= B",A >= B) //true
+console.log("A > B", A > B)//true
+console.log("A >= B", A >= B) //true
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Logical Operator
 // (Note:- in OR operator at least two expression/two condition) ((AND Operator &&),(OR Operator ||),(NOT Operator !))
@@ -162,7 +160,7 @@ let S = 5;
 
 // let cond1 = W > S //true() (using this also)
 // let cond2 = W === 6 //true (using this also)
-console.log("W && S",W > S && W === 6)//true 
+console.log("W && S", W > S && W === 6)//true 
 // console.log("W && S",cond1 && cond2)//true (using this also)
 
 // Logical OR Operator ||(Note:- in OR operator at least two expression/two condition)
@@ -178,30 +176,76 @@ console.log("!(W === '6') =", !(W === 6)) //true condition but false in NOT
 // Conditional Statements
 
 let AgeofSanket = 27;
-let Married =  'Engaged';
-if(AgeofSanket >= 30 &&  Married === 'Engaged'){
- console.log("Sanket Engaged at the age of 27")
-}else{
+let Married = 'Engaged';
+if (AgeofSanket >= 30 && Married === 'Engaged') {
+    console.log("Sanket Engaged at the age of 27")
+} else {
     console.log("Sanket Not Engaged at the age of 27")
 }
 
 let mode = "Dark";
 let message;
-if(mode == "Dark"){
+if (mode == "Dark") {
     message = alert('Hey I am Kajal Welcome to Javascript Session')
 }
-if(mode == "Light"){
-    message = alert('Hey I am Kajal Not Welcome to Javascript Session')
+if (mode == "Light") {
+    message = alert('Not Welcome to Javascript Session')
 }
 console.log(mode)
 
 const pro = {
-    proName :'Ball Pen',
-    rating :4.5,
-    offer :10,
-    price :270,
-    isDeal :true,
+    proName: 'Ball Pen',
+    rating: 4.5,
+    offer: 10,
+    price: 270,
+    isDeal: true,
 }
-console.log('Product Object',pro)
-console.log('price of Pen :',pro['price']+12)
+console.log('Product Object', pro)
+console.log('price of Pen :', pro['price'] + 12)
 console.log(pro.proName)
+
+//Looping Statements Start
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+console.log('Looping Statement End')
+// ...existing code...
+
+// Function to print odd numbers from 1 to a given limit
+for (i = 1; i <= 5; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+// Call the function with a limit, for example, 10
+// printOddNumbers(10);
+
+// ...existing code...
+
+// Function to print prime numbers up to a given limit
+function printPrimeNumbers(limit) {
+    for (let num = 2; num <= limit; num++) {
+        let isPrime = true;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            console.log(num);
+        }
+    }
+}
+
+// Call the function with a limit of 100
+printPrimeNumbers(100);
+
+// For Loop programm
+let sum = i;
+let n = 100;
+// WAP print the odd numbers from 1 to 10
+for (let count = 1; count % 2; count++) {
+    sum = sum + i;
+    console.log(n)
+}                                                                             
